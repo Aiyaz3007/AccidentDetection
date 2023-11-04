1e766c0d71c6ce548e0395700274a2c47f7b4e5f
Aiyaz3007 <maiyaz3007@gmail.com>
2023-11-04 07:20:22 +0530

    update requirements.txt

--- dataset.py
+++ dataset.py
@@ -5,5 +5,17 @@ try:
 except ModuleNotFoundError:
   print("kaggle not found! \nInstalling Kaggle")
   os.system("pip install kaggle")
+  
+import os
+import json
+
+kaggle_data = {"username":"aiyazm","key":"06b7b710ad258c1d23ccd666b1363bb2"}
+
+os.makedirs(".kaggle",exist_ok=True)
+with open(".kaggle/kaggle.json","w") as f:
+  json.dump(kaggle_data,f)
+  print("Done!")
+
+os.environ["KAGGLE_CONFIG_DIR"] = ".kaggle/kaggle.json"
 
 os.system("kaggle datasets download -d aiyazm/accidentdetection")
